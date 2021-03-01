8358c714ebc3ba233ec3792bfd4322c4f66ec258
UnknowN <44743518+seuminh@users.noreply.github.com>
2021-03-01 12:04:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const auth = require("./routes/auth");
 const users = require("./routes/users");
 // const branches = require("./routes/branches");
 // const reservations = require("./routes/reservations");
-const rooms = require("./routes/rooms");
+// const rooms = require("./routes/rooms");
 // const customers = require("./routes/customers");
 
 const app = express();
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "development") {
 // Router
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/users", users);
-app.use("/api/v1/rooms", rooms);
+// app.use("/api/v1/rooms", rooms);
 // app.use("/api/v1/branches", branches);
 // app.use("/api/v1/reservations", reservations);
 // app.use("/api/v1/customers", customers);
