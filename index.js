d7ad6114de55393feb835ae81ef63ebb5a350518
UnknowN <44743518+seuminh@users.noreply.github.com>
2021-03-01 12:02:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 const auth = require("./routes/auth");
 const users = require("./routes/users");
-// const branches = require("./routes/branches");
+const branches = require("./routes/branches");
 // const reservations = require("./routes/reservations");
 // const rooms = require("./routes/rooms");
 // const customers = require("./routes/customers");
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/users", users);
 // app.use("/api/v1/rooms", rooms);
-// app.use("/api/v1/branches", branches);
+app.use("/api/v1/branches", branches);
 // app.use("/api/v1/reservations", reservations);
 // app.use("/api/v1/customers", customers);
 
