ff051d212cb803f8a0d9c4e97a42713884e6633f
UnknowN <44743518+seuminh@users.noreply.github.com>
2021-03-01 11:47:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(
+app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
